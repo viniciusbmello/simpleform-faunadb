347fd7183dcc38ec068d0a886437a8c90bea62a3
Vinícius Baptista de Mello (Frimesa) <viniciusbmello@gmail.com>
2021-03-23 19:18:23 -0300

    Added ESLint and Prettier

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+
+const IndexPage: NextPage = () => {
+  return (
+    <>
+      <h1>simpleform-faunadb</h1>
+      <p>Example of NextJS Form with FaunaDB Database</p>
+    </>
+  )
+}
+
+export default IndexPage
